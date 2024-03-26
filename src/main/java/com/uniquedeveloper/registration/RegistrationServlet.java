b670b9a50a8547a1dbfd8d37e5393c5b35ebc0c0
Tuan Hiep Do <76672662+th2do@users.noreply.github.com>
2024-03-26 01:09:43 -0400

    Update RegistrationServlet.java

--- src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
+++ src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
@@ -12,7 +12,7 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import org.bouncycastle.crypto.generators.OpenBSDBCrypt;
 /**
  * Servlet implementation class RegistrationServlet
  */
@@ -24,16 +24,23 @@ public class RegistrationServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String user_name = request.getParameter("name");
 		String email = request.getParameter("email");
+		
+		// encrypt password 
 		String pass = request.getParameter("pass");
+		byte[] salt = {42, -10, 127, -50, 0, 64, -128, 33, -73, 18, 55, -91, 101, -29, -16, 83}; // random
+
+        // Hash the password with bcrypt
+        String hashedPassword = OpenBSDBCrypt.generate(pass.toCharArray(), salt, 12);
+		
 		String mobile = request.getParameter("contact");
 		RequestDispatcher dispatcher = null;
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LOGIN?useSSL=false", "root", "0989552898bB@");
-			PreparedStatement pst = con.prepareStatement("insert into users(user_name, pass, email, mobile) values(?,?,?,?) ");
+			PreparedStatement pst = con.prepareStatement("insert into users(user_name, hashed_pass, email, mobile) values(?,?,?,?) ");
 			pst.setString(1, user_name);
-			pst.setString(2, pass);
+			pst.setString(2, hashedPassword);
 			pst.setString(3, email);
 			pst.setString(4, mobile);
 			
