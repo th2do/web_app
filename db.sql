e04b5109b5c226b9ec3a86d6cbe386bf55bb6563
Tuan Hiep Do <76672662+th2do@users.noreply.github.com>
2024-03-26 01:09:22 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ drop table if exists users;
 create table users (
   id int primary key auto_increment,
   user_name varchar(50),
-  pass varchar(50),
+  hashed_pass varchar(60),
   email varchar(50),
   mobile varchar(20)
 );
